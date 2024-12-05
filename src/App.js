c756350daec67c3dd8713da5809911022c2eb749
VadimKorobec <_s_e_t__@ukr.net>
2024-12-05 13:33:48 +0200

    add some code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -6,11 +7,20 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-  
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+
+  useEffect(() => {
+    fetch("https://redux-8ce34-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
-       <Products />
+      <Products />
     </Layout>
   );
 }
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,9 +1,23 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartItem.module.css";
+import { addItemToCart, removeItemFromCart } from "../store/cartSlice";
 
-const CartItem = ({item}) => {
+const CartItem = ({ item }) => {
+  const dispatch = useDispatch();
 
-  
-  
+  const handleRemoveItem = (id) => {
+    dispatch(removeItemFromCart(id));
+  };
+
+  const handleAddItem = () => {
+    dispatch(
+      addItemToCart({
+        id: item.id,
+        title: item.title,
+        price: item.price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
@@ -11,7 +25,9 @@ const CartItem = ({item}) => {
         <h3>{item.title}</h3>
         <div className={classes.price}>
           ${item.totalPrice.toFixed(2)}
-          <span className={classes.itemprice}>(${item.price.toFixed(2)}/item)</span>
+          <span className={classes.itemprice}>
+            (${item.price.toFixed(2)}/item)
+          </span>
         </div>
       </header>
       <div className={classes.details}>
@@ -19,8 +35,8 @@ const CartItem = ({item}) => {
           x <span>{item.quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={() => handleRemoveItem(item.id)}>-</button>
+          <button onClick={handleAddItem}>+</button>
         </div>
       </div>
     </li>
