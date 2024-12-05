ba4768d763fcb7004d548b8c118984c40cedd2c1
VadimKorobec <_s_e_t__@ukr.net>
2024-12-05 14:36:44 +0200

    add some code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,80 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { showNotification } from "./components/store/uiSlice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  console.log(cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("https://redux-8ce34-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const response = await fetch(
+        "https://redux-8ce34-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed!");
+      }
+
+      dispatch(
+        showNotification({
+          status: "success",
+          title: "Success",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        showNotification({
+          status: "error",
+          title: "Error",
+          message: "Sending cart data failed!",
+        })
+      );
     });
-  }, [cart]);
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/store/uiSlice.js
+++ src/components/store/uiSlice.js
@@ -2,22 +2,32 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   cartIsVisible: false,
+  notification: null,
 };
 
 export const uiSlice = createSlice({
   name: "ui",
   initialState,
   reducers: {
-    toggle(state,) {
+    toggle(state) {
       return {
         ...state,
         cartIsVisible: !state.cartIsVisible,
       };
     },
-    
+    showNotification(state, action) {
+      return {
+        ...state,
+        notification: {
+          status: action.payload.status,
+          title: action.payload.title,
+          message: action.payload.message,
+        },
+      };
+    },
   },
 });
 
-export const {toggle} = uiSlice.actions;
+export const { toggle, showNotification } = uiSlice.actions;
 
-export const uiReducer =  uiSlice.reducer;
+export const uiReducer = uiSlice.reducer;
